649321db3e8c7854d510f125d78853e2816f19d2
Prateek Kher <prateek.kher@gmail.com>
2021-04-19 22:08:34 +0530

    Fixed Typo

--- src/app/shared/class/BaseService.ts
+++ src/app/shared/class/BaseService.ts
@@ -64,7 +64,7 @@ export abstract class BaseService {
         return [...this.data.value];
     }
 
-    getAsObervable(): Observable<TableRow[]> {
+    getAsObservable(): Observable<TableRow[]> {
         return this.data;
     }
 
