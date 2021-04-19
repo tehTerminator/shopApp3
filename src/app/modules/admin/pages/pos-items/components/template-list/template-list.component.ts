1d393a132861d8b77c1ad250b004055ba47859bb
Prateek Kher <prateek.kher@gmail.com>
2021-04-19 22:09:33 +0530

    File Created

--- src/app/modules/admin/pages/pos-items/components/template-list/template-list.component.ts
+++ src/app/modules/admin/pages/pos-items/components/template-list/template-list.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { PosItemService } from './../../../../../../shared/services/posItem';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { PosItemService } from './../../../../../../shared/services/posItem/pos-item.service';
 import { ProductService } from './../../../../../../shared/services/product/product.service';
 import { LedgerService } from './../../../../../../shared/services/ledger/ledger.service';
+import { Ledger, PosItem, PosItemTemplate, Product } from '../../../../../../shared/collection';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-template-list',
   templateUrl: './template-list.component.html',
   styleUrls: ['./template-list.component.css']
 })
-export class TemplateListComponent implements OnInit {
+export class TemplateListComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() pos = 0;
+  templates: PosItemTemplate[] = [];
+  private sub: Subscription = new Subscription();
 
   constructor(
     private productService: ProductService,
@@ -19,6 +24,67 @@ export class TemplateListComponent implements OnInit {
   ngOnInit(): void {
     this.productService.init();
     this.ledgerService.init();
+    this.posItemService.init();
+
+    this.sub = this.posItems.subscribe(
+      (items) => {
+        try {
+          const temp = items.find(x => x.id === this.pos).templates;
+          if (temp === undefined) {
+            this.templates = [];
+          } else {
+            this.templates = temp;
+          }
+        } catch (e) {
+          console.log('PosItem Not Found');
+          this.templates = [];
+        }
+      }
+    );
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const change = changes.pos;
+
+    if (change.currentValue !== change.previousValue) {
+      this.pos = change.currentValue;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+
+  get posItems(): Observable<PosItem[]> {
+    return this.posItemService.getAsObservable() as Observable<PosItem[]>;
+  }
+
+  getTitle(template: PosItemTemplate): string {
+    if (template.kind === 'PRODUCT') {
+      return this.getProductName(template.item_id);
+    }
+    return this.getledgerName(template.item_id);
+  }
+
+  private getProductName(id: number): string {
+    let productName = '';
+    try {
+      productName = (this.productService.getElementById(id) as Product).title;
+    } catch (e) {
+      productName = 'NOT FOUND';
+    }
+    return productName;
+  }
+
+  private getledgerName(id: number): string {
+    let ledgerName = '';
+    try {
+      ledgerName = (this.ledgerService.getElementById(id) as Ledger).title;
+    } catch (e) {
+      ledgerName = 'NOT FOUND';
+    }
+    return ledgerName;
   }
 
 }
