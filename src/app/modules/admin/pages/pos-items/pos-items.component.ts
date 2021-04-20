89cf389e3c2c8230bf61cab69ec40cce20af366b
Prateek Kher <prateek.kher@gmail.com>
2021-04-20 14:36:29 +0530

    Removed Unnecessary StateService

--- src/app/modules/admin/pages/pos-items/pos-items.component.ts
+++ src/app/modules/admin/pages/pos-items/pos-items.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { PosItemService } from '../../../../shared/services/posItem/pos-item.service';
-import { PosFormState, PosFormStateService } from './services/pos-form-state.service';
 
 @Component({
   selector: 'app-pos-items',
@@ -9,16 +7,8 @@ import { PosFormState, PosFormStateService } from './services/pos-form-state.ser
 })
 export class PosItemsComponent implements OnInit {
 
-  public readonly CREATING_POS_ITEM = PosFormState.CREATING_POS_ITEM;
-  public readonly CREATING_TEMPLATE = PosFormState.CREATING_TEMPLATE;
-  public readonly UPDATING_POS_ITEM = PosFormState.UPDATING_POS_ITEM;
-  public readonly SELECTING_TEMPLATE = PosFormState.SELECTING_TEMPLATE;
-  public readonly UPDATING_TEMPLATE = PosFormState.UPDATING_TEMPLATE;
 
-  constructor(
-    private posItemService: PosItemService,
-    private formStateService: PosFormStateService
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
