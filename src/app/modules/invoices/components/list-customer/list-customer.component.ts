f924c8cd8c86905c62b75ad210018348d46ee059
Prateek Kher <prateek.kher@gmail.com>
2021-04-21 17:34:11 +0530

    Implementation Completed

--- src/app/modules/invoices/components/list-customer/list-customer.component.ts
+++ src/app/modules/invoices/components/list-customer/list-customer.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Customer } from '../../../../shared/collection';
+import { CustomerService } from '../../services/customer.service';
+import { InvoiceStoreService } from './../../services/invoice-store.service';
 
 @Component({
   selector: 'app-list-customer',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListCustomerComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private customerService: CustomerService,
+    private store: InvoiceStoreService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  get customers(): Observable<Customer[]> {
+    return this.customerService.getAsObservable() as Observable<Customer[]>;
+  }
+
+  onSelect(customer: Customer): void {
+    this.store.customer = customer;
+    this.router.navigate(['/invoices', 'list-items']);
+  }
 }
