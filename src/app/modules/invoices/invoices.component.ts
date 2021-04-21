ea03b78579940578772767f05ec2bddd8cf813cc
Prateek Kher <prateek.kher@gmail.com>
2021-04-21 19:39:49 +0530

    Initialized services for later use

--- src/app/modules/invoices/invoices.component.ts
+++ src/app/modules/invoices/invoices.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LedgerService } from '../../shared/services/ledger/ledger.service';
+import { PosItemService } from '../../shared/services/posItem/pos-item.service';
+import { ProductService } from '../../shared/services/product/product.service';
 
 @Component({
   selector: 'app-invoices',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoicesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ledgerService: LedgerService,
+    private productService: ProductService,
+    private posItemService: PosItemService
+  ) { }
 
   ngOnInit(): void {
+    // Initialize Services to Be Used Later
+    this.ledgerService.init();
+    this.productService.init();
+    this.posItemService.init();
   }
 
 }
