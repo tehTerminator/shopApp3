738fee2c30ff6fa084d4ba342c12c94cd52cce54
Prateek Kher <prateek.kher@gmail.com>
2021-04-21 23:44:43 +0530

    Implementation Completed 50%

--- src/app/modules/invoices/components/preview-invoice/preview-invoice.component.ts
+++ src/app/modules/invoices/components/preview-invoice/preview-invoice.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { Invoice, Transaction } from '../../../../shared/collection';
+import { InvoiceStoreService } from './../../services/invoice-store.service';
 
 @Component({
   selector: 'app-preview-invoice',
   templateUrl: './preview-invoice.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class PreviewInvoiceComponent implements OnInit {
-
-  constructor() { }
+export class PreviewInvoiceComponent implements OnInit, OnDestroy {
+  invoice: Invoice = { ... this.store.baseInvoice };
+  sub: Subscription = new Subscription();
+  constructor(private store: InvoiceStoreService) { }
 
   ngOnInit(): void {
+    this.sub = this.store.invoice
+    .subscribe(val => this.invoice = val);
   }
 
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  rowAmount(transaction: Transaction) {
+    return (transaction.quantity * transaction.rate ) * (1 - transaction.discount / 100);
+  }
 }
