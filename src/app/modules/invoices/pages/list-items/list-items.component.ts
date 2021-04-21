09bb293c97503ad4358cc96a94982cc0c6b2f584
Prateek Kher <prateek.kher@gmail.com>
2021-04-21 19:41:17 +0530

    Using Array instead of Observable

--- src/app/modules/invoices/pages/list-items/list-items.component.ts
+++ src/app/modules/invoices/pages/list-items/list-items.component.ts
@@ -3,8 +3,8 @@ import { Router } from '@angular/router';
 import { LedgerService } from './../../../../shared/services/ledger/ledger.service';
 import { ProductService } from './../../../../shared/services/product/product.service';
 import { PosItemService } from './../../../../shared/services/posItem/pos-item.service';
-import { from, merge, Observable, } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { asyncScheduler, concat, EMPTY, from, merge, Observable, scheduled, } from 'rxjs';
+import { concatAll, concatMap, map, mergeAll, mergeMap, take } from 'rxjs/operators';
 import { Ledger, PosItem, Product } from '../../../../shared/collection';
 import { InvoiceStoreService } from '../../services/invoice-store.service';
 import { NotificationService } from '../../../../shared/services/notification/notification.service';
@@ -31,34 +31,44 @@ export class ListItemsComponent implements OnInit {
     this.posItemService.init();
   }
 
-  get items(): Observable<GeneralItem[]> {
-    return merge(
-      this.ledgerService.getAsObservable() as Observable<Ledger[]>,
-      this.productService.getAsObservable() as Observable<Product[]>,
-      this.posItemService.getAsObservable() as Observable<PosItem[]>,
-    ).pipe(
-      map(
-        list => {
-          if (list.length === 0) {
-            return [];
-          }
-          const newList: GeneralItem[] = [];
-          list.forEach((item: Product | Ledger | PosItem) => {
-            let type = ItemType.PRODUCT;
-            if (this.ledgerService.isInstanceOfLedger(item)) {
-              type = ItemType.LEDGER;
-            } else if (this.posItemService.isInstanceOfPosItem(item)) {
-              type = ItemType.POSITEM;
-            }
-            newList.push({
-              id: item.id,
-              title: item.title,
-              type,
-            });
-          });
-          return newList;
-        })
-    );
+  get products(): GeneralItem[] {
+    const list = this.productService.getAsList() as Product[];
+    return this.mapListToGeneralItemList(list);
+  }
+
+  get ledgers(): GeneralItem[] {
+    const list = this.ledgerService.getAsList() as Product[];
+    return this.mapListToGeneralItemList(list);
+  }
+
+  get posItems(): GeneralItem[] {
+    const list = this.posItemService.getAsList() as Product[];
+    return this.mapListToGeneralItemList(list);
+  }
+
+  get items(): GeneralItem[] {
+    return [...this.products, ...this.posItems, ...this.ledgers];
+  }
+
+  private mapListToGeneralItemList(list: Ledger[] | Product[] | PosItem[]): GeneralItem[] {
+    if (list.length === 0) {
+      return [];
+    }
+    const newList: GeneralItem[] = [];
+    list.forEach((item: Product | Ledger | PosItem) => {
+      let type = ItemType.PRODUCT;
+      if (this.ledgerService.isInstanceOfLedger(item)) {
+        type = ItemType.LEDGER;
+      } else if (this.posItemService.isInstanceOfPosItem(item)) {
+        type = ItemType.POSITEM;
+      }
+      newList.push({
+        id: item.id,
+        title: item.title,
+        type,
+      });
+    });
+    return newList;
   }
 
   onSelect(item: GeneralItem): void {
