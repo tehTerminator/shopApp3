dd1dcf76afb660343ee9d95d64a92ff650fd8157
Prateek Kher <prateek.kher@gmail.com>
2021-05-22 21:10:42 +0530

    Added Header IMage

--- src/app/modules/home/components/default/default.component.ts
+++ src/app/modules/home/components/default/default.component.ts
@@ -2,17 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-default',
-  template: `
-    <div class="card">
-      <div class="card-body">
-        <h1 class="fs-1 text-center m-5 p-5">
-          Welcome to Shop App
-        </h1>
-      </div>
-    </div>
-  `,
-  styles: [
-  ]
+  templateUrl: './default.component.html',
+  styleUrls: ['./default.component.css']
 })
 export class DefaultComponent implements OnInit {
 
