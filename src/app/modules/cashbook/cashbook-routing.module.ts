313530fb2cd1dafa823b92593c2fb988c6fbf299
Prateek Kher <prateek.kher@gmail.com>
2021-06-27 14:46:21 +0530

    Added Default Route

--- src/app/modules/cashbook/cashbook-routing.module.ts
+++ src/app/modules/cashbook/cashbook-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CashbookComponent } from './cashbook.component';
 import { VoucherFormComponent } from './pages/voucher-form/voucher-form.component';
 
 const routes: Routes = [
-  { path: '', component: CashbookComponent },
   { path: 'voucher', component: VoucherFormComponent },
   { path: 'statement', loadChildren: () => import('./pages/statement/statement.module').then(m => m.StatementModule) },
-  { path: 'balance', loadChildren: () => import('./pages/ledger-balance/ledger-balance.module').then(m => m.LedgerBalanceModule) }
+  { path: 'balance', loadChildren: () => import('./pages/ledger-balance/ledger-balance.module').then(m => m.LedgerBalanceModule) },
+  { path: '**', redirectTo: 'voucher', pathMatch: 'full' }
 ];
 
 
