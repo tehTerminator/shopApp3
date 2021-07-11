6cf8e4d38286b95446f621071e628634e9ce0675
Prateek Kher <prateek.kher@gmail.com>
2021-07-11 15:41:11 +0530

    No More Multiple Request While Fetching balance

--- src/app/modules/cashbook/pages/ledger-balance/auto-set-button/auto-set-button.component.ts
+++ src/app/modules/cashbook/pages/ledger-balance/auto-set-button/auto-set-button.component.ts
@@ -1,58 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
-import { Ledger } from '../../../../../shared/collection';
-import { LedgerService } from '../../../../../shared/services/ledger/ledger.service';
+import { Component } from '@angular/core';
+import { ApiService } from '../../../../../shared/services/api/api.service';
+import { LedgerBalanceService } from '../ledger-balance.service';
 
 @Component({
     selector: 'app-auto-set-buttons',
     templateUrl: './auto-set-button.component.html',
     styles: []
 })
-export class AutoSetButtons implements OnInit {
-    idList: number[] = [];
-    private cursor = 0;
-    private initialized = false;
+export class AutoSetButtons {
 
-    ngOnInit(): void {
-        this.fetchUniqueId();
-    }
-
-    private fetchUniqueId(): void {
-        const ledgers = this.ledgerService.getAsList() as Ledger[];
-        const bankAndCash = ledgers.filter(
-            item => item.kind === 'BANK' || item.kind === 'CASH'
-        );
-        bankAndCash.forEach(item => {
-            this.idList.push(item.id);
-        });
-
-        this.initialized = this.idList.length > 0;
-    }
-
-    setOpening(): void {
-        if (!this.initialized) {
-            this.fetchUniqueId();
-        }
-
-        if (this.idList.length === 0) {
-            return;
-        }
-
-        if (this.cursor === this.idList.length) {
-            return;
-        }
-
-        this.ledgerService.updateBalance(this.idList[this.cursor])
-        .pipe(retry(2))
+    updateBalance(): void {
+        this.api.update<any>('balance', {})
         .subscribe(
-            () => { this.cursor++; this.setOpening(); }
+            (response => this.store.accountBalance.next(response))
         );
     }
-
-    get ledgers(): Observable<Ledger[]> {
-        return this.ledgerService.getAsObservable() as Observable<Ledger[]>;
-    }
-
-    constructor(private ledgerService: LedgerService) {}
+    constructor(private api: ApiService, private store: LedgerBalanceService) {}
 }
--- src/app/modules/cashbook/pages/ledger-balance/ledger-balance-list/ledger-balance-list.component.ts
+++ src/app/modules/cashbook/pages/ledger-balance/ledger-balance-list/ledger-balance-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { LedgerBalance, LedgerBalanceService } from '../ledger-balance.service';
 import { getCurrentDateString } from './../../../../../shared/functions';
 
@@ -9,16 +9,27 @@ import { getCurrentDateString } from './../../../../../shared/functions';
   templateUrl: './ledger-balance-list.component.html',
   styles: ['']
 })
-export class LedgerBalanceListComponent implements OnInit {
+export class LedgerBalanceListComponent implements OnInit, OnDestroy {
   dateField = new FormControl();
+  hasData = false;
+  sub = new Subscription();
   constructor(private store: LedgerBalanceService) { }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.dateField.setValue(getCurrentDateString());
+    this.updateList();
+    this.sub = this.store.accountBalance.subscribe(
+      (data => this.hasData = data.length > 0)
+    );
   }
 
   get data(): Observable<LedgerBalance[]> {
     return this.store.accountBalance;
   }
+
   updateList = () => this.store.fetchData(this.dateField.value);
 }
