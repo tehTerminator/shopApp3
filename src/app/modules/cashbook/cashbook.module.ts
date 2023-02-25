0b16d9e390583a498dae557e0568a8ba3362098e
Prateek Kher <prateek.kher@gmail.com>
2023-02-25 22:45:00 +0530

    Updated Angular

--- src/app/modules/cashbook/cashbook.module.ts
+++ src/app/modules/cashbook/cashbook.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreModule } from './../core/core.module';
-import { MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatLegacyAutocompleteModule as MatAutocompleteModule} from '@angular/material/legacy-autocomplete';
 
 import { CashbookRoutingModule } from './cashbook-routing.module';
 import { CashbookComponent } from './cashbook.component';
--- src/app/modules/invoices/pages/search-invoice/search-invoice.module.ts
+++ src/app/modules/invoices/pages/search-invoice/search-invoice.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedComponentModule } from '../../shared-components/shared-components.module';
 import { InvoiceListComponent } from './components/invoice-list/invoice-list.component';
