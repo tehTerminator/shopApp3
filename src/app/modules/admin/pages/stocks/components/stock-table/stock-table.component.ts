505f9c9839885177f744f37298b3d73c8a00265b
Prateek Kher <prateek.kher@gmail.com>
2023-03-16 18:40:53 +0530

    Created Stock-Table Component

---
+++ src/app/modules/admin/pages/stocks/components/stock-table/stock-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-table',
+  templateUrl: './stock-table.component.html',
+  styleUrls: ['./stock-table.component.css']
+})
+export class StockTableComponent {
+  constructor()
+}
