fea2db236a9b6cd51b850a5c0fe65a4219656de0
Prateek Kher <prateek.kher@gmail.com>
2023-03-16 18:41:14 +0530

    Added Stock-Table Component

--- src/app/modules/admin/pages/stocks/stocks.module.ts
+++ src/app/modules/admin/pages/stocks/stocks.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { StocksComponent } from './stocks.component';
 import { StockFormComponent } from './components/stock-form/stock-form.component';
 import { CoreModule } from './../../../../modules/core/core.module';
+import { StockTableComponent } from './components/stock-table/stock-table.component';
+import { MatTableModule } from '@angular/material/table';
 
 
 const routes: Routes = [
@@ -13,9 +15,11 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     StocksComponent,
-    StockFormComponent
+    StockFormComponent,
+    StockTableComponent
   ],
   imports: [
+    MatTableModule,
     CoreModule,
     CommonModule,
     RouterModule.forChild(routes)
