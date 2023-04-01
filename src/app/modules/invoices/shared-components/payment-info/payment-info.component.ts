a4fcfbf9c9184b97db5c2179e8d916a7d6532ed9
Prateek Kher <prateek.kher@gmail.com>
2023-04-01 23:31:59 +0530

    Payment Info now Works

--- src/app/modules/invoices/shared-components/payment-info/payment-info.component.ts
+++ src/app/modules/invoices/shared-components/payment-info/payment-info.component.ts
@@ -1,61 +1,34 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ApiService } from '../../../../shared/services/api/api.service';
-import { PaymentInfo } from './../../../../shared/interface/invoice.interface';
 import { BehaviorSubject } from 'rxjs';
 import { InvoiceStoreService } from '../../services/invoice-store.service';
 import { LedgerService } from '../../../../shared/services/ledger/ledger.service';
+import { Voucher } from '../../../../shared/collection';
 
 @Component({
   selector: 'app-payment-info',
   templateUrl: './payment-info.component.html',
 })
-export class PaymentInfoComponent implements OnChanges {
-  @Input('invoiceId') id = 0;
+export class PaymentInfoComponent {
+  private _invoiceId = 0;
+  paymentInfo = new BehaviorSubject<Voucher[]>([]);
+  @Input('invoice') 
+  set invoiceId(id: number) {
+    this._invoiceId = id;
+    this.fetchVoucherInfo();
+  }
 
-//   paymentInfo = new BehaviorSubject<PaymentInfo[]>([]);
-
-//   ngOnChanges(changes: SimpleChanges): void {
-//     if (changes.id.currentValue !== changes.id.previousValue) {
-//         console.log(id)
-//       this.api
-//         .select<PaymentInfo[]>(
-//           'invoices/paymentInfo/' + changes.id.currentValue
-//         )
-//         .subscribe(
-//           (data) => this.paymentInfo.next(data),
-//           (err) => {
-//             this.paymentInfo.next([]);
-//             console.error(err);
-//           }
-//         );
-//     }
-//   }
-
-//   get amountPaid(): number {
-//     let info = this.paymentInfo.value;
-//     if (info.length === 0) {
-//         return 0;
-//     }
-
-//     let amount = 0;
-//     info.forEach(function(item) {
-//         amount += item.amount;
-//     });
-
-//     return amount;
-//   }
-
-//   get paymentStatus(): string {
-//     if (this.amountPaid === 0) {
-//         return 'UNPAID';
-//     } 
-
-//     if (this.amountPaid === this.invoiceStore.invoice.value.amount) {
-//         return 'PAID';
-//     }
-
-//     return 'OVERPAID';
-//   }
+  private fetchVoucherInfo() {
+    this.api.select<Voucher[]>('invoices/paymentInfo', {id: this._invoiceId.toString()})
+    .subscribe(
+      (data) => this.paymentInfo.next(data),
+      (err) => {
+        console.error(err);
+        this.paymentInfo.next([]);
+      }
+    )
+  }
+  
 
   constructor(private api: ApiService,
     private invoiceStore: InvoiceStoreService,
