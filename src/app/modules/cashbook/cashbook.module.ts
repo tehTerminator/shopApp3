3e37e1c2173fd03c0a5b58dcb930962193120e22
Prateek Kher <prateek.kher@gmail.com>
2023-04-02 14:23:20 +0530

    Updated Old Imports

--- src/app/modules/cashbook/cashbook.module.ts
+++ src/app/modules/cashbook/cashbook.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreModule } from './../core/core.module';
-import { MatLegacyAutocompleteModule as MatAutocompleteModule} from '@angular/material/legacy-autocomplete';
+import { MatAutocompleteModule} from '@angular/material/autocomplete';
 
 import { CashbookRoutingModule } from './cashbook-routing.module';
 import { CashbookComponent } from './cashbook.component';
